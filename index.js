fca7806e89556904afbb93f220a9415d74a38a26
Satyam Kumar <ksatyam433@gmail.com>
2022-11-18 10:12:42 +0530

    Minor CHanges in Index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require("express")
-const path = require("path")
-const mongoose = require("mongoose")
-const cors = require("cors")
-require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 // Mongoose to MongoDB
-const uri = process.env.MONGODB_URI || "mongodb+srv://sarthakskumar:9742913699@notes-web-app.x3ycmcm.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const uri = process.env.MONGODB_URI || 'mongodb+srv://sarthakskumar:9742913699@notes-web-app.x3ycmcm.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log("Connected to remote database (MongoDB)")
+    console.log('Connected to remote database (MongoDB)');
 });
 
 // Variables
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // Routes
-const notesRoutes = require("./routes/notes")
-app.use("/api", notesRoutes)
+const notesRoutes = require('./routes/notes');
+app.use('/api', notesRoutes);
 
 // For deployment
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log("\n-----------NOTE-TAKING WEB APP-----------")
-  console.log("   CSE Sem-3 Web Technologies Project\n")
-  console.log(":) Sarthak S Kumar\n:) Sathyam Kumar\n:) Sathish Kumar G")
-  console.log("-----------------------------------------")
-  console.log("Express server running on port 5000 (Local Deployment PORT)")
-})
+    console.log('\n-----------NOTE-TAKING WEB APP-----------');
+    console.log('   CSE Sem-3 Web Technologies Project\n');
+    console.log(':) Sarthak S Kumar\n:) Satyam Kumar\n:) Sathish Kumar G');
+    console.log('-----------------------------------------');
+    console.log('Express server running on port 5000 (Local Deployment PORT)');
+});
